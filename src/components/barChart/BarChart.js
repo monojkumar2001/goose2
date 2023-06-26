56b027412f3353d867e954d92e3bc17908fb24c0
Ripon <Ripon20244@gmail.com>
2023-06-26 21:43:25 +0600

    first commit

--- src/components/barChart/BarChart.js
+++ src/components/barChart/BarChart.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import {Bar} from "react-chartjs-2";
-function BarChart({chartData}) {
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+function BarChart({ chartData }) {
   return (
     <>
-    <Bar data={chartData} />
+      <Bar data={chartData} />
     </>
-  )
+  );
 }
 
-export default BarChart
+export default BarChart;
