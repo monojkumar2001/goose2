646464c59ea0fcf2938b28a39b8362700d4c861d
Ripon <Ripon20244@gmail.com>
2023-06-28 12:58:42 +0600

    first commit

--- src/Context/useContext.js
+++ src/Context/useContext.js
@@ -566,11 +566,11 @@ export const ContextProvider = ({ children }) => {
 
   upland = getUrlParameter("ref") ? getUrlParameter("ref") : referrer;
 
-  useEffect(() => {
-    if (!walletConnected) {
-      getTronweb();
-    }
-  }, [walletConnected]);
+  // useEffect(() => {
+  //   if (!walletConnected) {
+  //     getTronweb();
+  //   }
+  // }, [walletConnected]);
 
   useEffect(() => {
     copeRef();
